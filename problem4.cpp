5b188d7e34347bac17eaf54f083784919b139fea
TylerOuellette6 <touel912@mail.endicott.edu>
2019-08-12 15:54:50 -0400

    Comments for Problem 4

--- problem4.cpp
+++ problem4.cpp
@@ -4,6 +4,14 @@
 #include <string>
 using namespace std;
 
+/**
+PROBLEM:
+A palindromic number reads the same both ways. 
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
 int main(){
 	int tempProduct;
 	int largestPalindrome = 0;
@@ -11,6 +19,7 @@ int main(){
 
 	// Iterate through all three-digit numbers
 	for(int i = 999; i > 99; i--){
+		// No need to check dupes, start j at i to avoid dupes
 		for(int j = i; j > 99; j--){
 			tempProduct = i * j;
 			// Convert to string in order to check each spot in the int
@@ -19,6 +28,8 @@ int main(){
 			string productStr = str1.str();
 
 			// Compare the "k" place with the "negative k" place
+			// Here > _----_ < And here
+			// And then move both "pointers" in one 
 			for(int k = 0; k < productStr.length()/2; k++){
 				// If the spots aren't equal, it's not a palindrome
 				if(productStr[k] != productStr[productStr.length()-(k+1)]){
