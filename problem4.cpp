14d709c14d0d86c2b1ee61298f04a89f4801d86c
TylerOuellette6 <touel912@mail.endicott.edu>
2019-08-12 15:29:23 -0400

    Problem 4

--- problem4.cpp
+++ problem4.cpp
@@ -6,31 +6,36 @@ using namespace std;
 
 int main(){
 	int tempProduct;
-	int largestPalindrome;
-	bool isPalindrome = false;
+	int largestPalindrome = 0;
+	bool notPalindrome = false;
 
+	// Iterate through all three-digit numbers
 	for(int i = 999; i > 99; i--){
 		for(int j = i; j > 99; j--){
 			tempProduct = i * j;
+			// Convert to string in order to check each spot in the int
 			ostringstream str1;
 			str1 << tempProduct;
 			string productStr = str1.str();
-			cout << productStr \
-			<< "\tLength: " << productStr.length()
-			<< "\tFirst: " << productStr[0] 
-			<< "\tLast: " <<  productStr[productStr.length()-1] << endl;
-			// for(int k = 0; k < productStr.length()/2; k++){
-			// 	if(productStr[0] != productStr[productStr.length()-(k+1)]){
-			// 		break;
-			// 	}
-			// 	isPalindrome = true;
-			// }
-			// if(isPalindrome == true){
-			// 	cout << i << " and " << j << " make " << productStr << endl;
-			// }
+
+			// Compare the "k" place with the "negative k" place
+			for(int k = 0; k < productStr.length()/2; k++){
+				// If the spots aren't equal, it's not a palindrome
+				if(productStr[k] != productStr[productStr.length()-(k+1)]){
+					notPalindrome = true;
+					break;
+				}
+				notPalindrome = false;
+			}
+			// If a palindrome is found, check if it's the largest one and store it
+			if(!notPalindrome && largestPalindrome < tempProduct){
+				largestPalindrome = tempProduct;
+			}
+			// Reset 
 			tempProduct = 0;
 		}
 	}
+	cout << largestPalindrome;
 
 	return 0;
 }
