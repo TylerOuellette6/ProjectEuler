e3a5c07d6c68c70abe9934e065349b76e8ea0064
TylerOuellette6 <touel912@mail.endicott.edu>
2019-08-12 15:31:25 -0400

    Comments for Problem 1

--- problem1.cpp
+++ problem1.cpp
@@ -2,11 +2,22 @@
 #include <fstream>
 using namespace std;
 
+/**
+PROBLEM:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 int main(){
 	int sum = 0;
 
+	// Go through all numbers less than 1000
 	for(int i = 0; i < 1000; i++){
+		// Make sure they're divisible by 3 AND 5
 		if(i % 3 == 0 || i % 5 == 0){
+			// Add to the overall sum
 			sum += i;
 		}
 	}
